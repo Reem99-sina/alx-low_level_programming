b5638ea73dec5783daed9fcaee8656f8aecdb865
Reem99-sina <reemsina2@gmail.com>
2023-02-16 11:23:00 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints the phrase with proper grammar,
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
