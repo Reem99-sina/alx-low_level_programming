c47b8ca9865a9e9e4805911a24de185f1162c4df
Reem99-sina <reemsina2@gmail.com>
2023-02-21 11:16:26 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+*main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+*Return: always (0)
+*/
+int main(void)
+{
+long num1, num2, next;
+long i, sum;
+
+num1 = 1;
+num2 = 2;
+next = num1 + num2;
+if (next % 2 == 0)
+{
+sum += next;
+}
+if (num1 % 2 == 0)
+{
+sum += num1;
+}
+if (num2 % 2 == 0)
+{
+sum += num2;
+}
+for (i = 3; i < 4000000; i++)
+{
+num1 = num2;
+num2 = next;
+next = num1 + num2;
+if (next % 2 == 0)
+{
+sum += next;
+}
+}
+printf("%ld\n", sum);
+return (0);
+}
