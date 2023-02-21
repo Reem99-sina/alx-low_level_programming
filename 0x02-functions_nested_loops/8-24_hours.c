8ee11b25e74e6e201b1d71f8d1f677b9b3f8db5f
Reem99-sina <reemsina2@gmail.com>
2023-02-21 05:16:54 -0800

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,12 @@ for (i = 0; i < 24; i++)
 {
 for (j = 0; j < 60; j++)
 {
-printf("%d:%d\n", i, j);
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
 }
 }
 }
