0865ddb53ea4e1a028ed257258d43278ecf53a0f
Reem99-sina <reemsina2@gmail.com>
2023-02-22 03:00:08 -0800

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,6 +12,7 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
+largest=c;
 if (a > b && a > c)
 {
 largest = a;
@@ -19,10 +20,6 @@ largest = a;
 else if (b > a && b > c)
 {
 largest = b;
-}
-else
-{
-largest = c;
 }
 
 return (largest);
