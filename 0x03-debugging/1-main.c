70e22dc66b11780eab8e1671d691d2f2320791b6
Reem99-sina <reemsina2@gmail.com>
2023-02-22 01:28:54 -0800

    debugg

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /**
- *while (i < 10)
- *{
- *putchar(i + '0');
- *}*/
-
+*while (i < 10)
+*{
+*putchar(i + '0');
+*}
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
