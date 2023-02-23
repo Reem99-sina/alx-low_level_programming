b4eb8c2e196e99401366b32961a0466624d01730
Reem99-sina <reemsina2@gmail.com>
2023-02-23 08:11:37 -0800

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,23 @@ for (i = 1; i < 101; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 3 != 0 && i % 5 == 0)
 {
-printf("Bizz ");
+printf("Bizz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBizz ");
+printf("FizzBizz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
 }
 }
 printf("\n");
