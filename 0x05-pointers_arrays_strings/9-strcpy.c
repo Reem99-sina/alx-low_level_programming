1aeb76de7d2ed28099c5d583db0985d8acd67154
Reem99-sina <reemsina2@gmail.com>
2023-02-27 13:06:34 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <string.h>
+/**
+ * _strcpy - copies the string  src to dest
+ *@dest:string
+ *@src: string
+ * Return:no return
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (src);
+}
