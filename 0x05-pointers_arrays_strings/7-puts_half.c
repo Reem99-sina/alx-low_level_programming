ccaed46a92f5611ba51a70fda72faa2094d9b626
Reem99-sina <reemsina2@gmail.com>
2023-02-27 10:08:53 -0800

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half -  prints half of a string
+ *@str:string to print half of it
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+int slen = strlen(str);
+int i;
+
+for (i = slen / 2; i <= slen; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ void print_rev(char *s);
 char *strrev(char *str);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
