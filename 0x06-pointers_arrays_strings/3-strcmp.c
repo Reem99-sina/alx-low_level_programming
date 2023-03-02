aa093a32e07c513f02e0ad299c7dba308a890fbf
Reem99-sina <reemsina2@gmail.com>
2023-03-02 06:48:49 -0800

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ *@s1:string
+ *@s2:string
+ * Return: result
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
