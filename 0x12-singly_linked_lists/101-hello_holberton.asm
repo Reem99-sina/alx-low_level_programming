4c37d9b990d68123f045d8bfb09f025f26a169b1
Reem99-sina <reemsina2@gmail.com>
2023-03-30 16:11:51 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,12 @@
-section .data
-    fmt: db "Hello, Holberton!", 0Ah, 0
+	global 	main
+	extern 	printf
 
-section .text
-    global main
-
-    extern printf
-
-    main:
-        push    rbp
-        mov     rbp, rsp
-        mov     rdi, fmt
-        xor     eax, eax
-        call    printf
-        mov     eax, 0
-        pop     rbp
-        ret
+	section .text
+main:
+	mov	rdi, format
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+format:
+	db "Hello, Holberton", 10, 0
