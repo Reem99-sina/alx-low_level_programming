5f004900b7e16740de688b87bd1ed8d0e181615d
Reem99-sina <reemsina2@gmail.com>
2023-04-03 14:27:54 -0700

    length

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,15 +10,17 @@
  */
 int pop_listint(listint_t **head)
 {
-listint_t *tmp;
-int number;
-if (*head == NULL)
-{
-return (0);
-}
-tmp = *head;
-ret = (*head)->n;
-*head = (*head)->next;
-free(tmp);
-return (NULL);
+	listint_t *tmp;
+	int ret;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (ret);
 }
