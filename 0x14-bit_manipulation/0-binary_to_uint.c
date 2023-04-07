214ba582ccdd128792113e22d2d815aeeac9f2db
Reem99-sina <reemsina2@gmail.com>
2023-04-07 06:37:13 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int len = 0;
+unsigned int num = 0;
+int len = 0;
 
-	if (b[len] == '\0')
-		return (0);
+if (b[len] == '\0')
+return (0);
 
-	while ((b[len] == '0') || (b[len] == '1'))
-	{
-		num <<= 1;
-		num += b[len] - '0';
-		len++;
-	}
+while (b[len])
+{
+if (b[len] != '1' && b[len] != '0')
+{
+return (0);
+}
+num <<= 1;
+num += b[len] - '0';
+len++;
+}
 
-	return (num);
+return (num);
 }
