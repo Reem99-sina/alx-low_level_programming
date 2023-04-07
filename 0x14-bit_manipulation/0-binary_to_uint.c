eccadb1f5c38d5b20bdd0ca83a14254901edded8
Reem99-sina <reemsina2@gmail.com>
2023-04-07 07:15:21 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,14 +8,32 @@
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  *         Otherwise - the converted number.
  */
+int check_valid_string(const char *b)
+{
+if (b == NULL)
+return (0);
+
+while (*b)
+{
+if (*b != '1' && *b != '0')
+return (0);
+b++;
+}
+return (1);
+}
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int num = 0;
 int len = 0;
 
-if (b[len] == '\0' || !b[len])
+if (!check_valid_string(b))
+{
 return (0);
-
+}
+if ((b[len] == '\0') || (b == NULL))
+{
+return (0);
+}
 while (b[len])
 {
 if (b[len] != '1' && b[len] != '0')
@@ -26,6 +44,6 @@ num <<= 1;
 num += b[len] - '0';
 len++;
 }
-
 return (num);
 }
+
