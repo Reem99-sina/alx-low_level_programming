ef5dc7fe5f6744f9908e73dbb8da429658b3ea60
Reem99-sina <reemsina2@gmail.com>
2023-04-07 07:35:08 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include <stddef.h>
+
 /**
  * check_valid_string - checks if a string has only 0's and 1's
  * @b: string to be checked
  *
  * Return: 1 if string is valid, 0 otherwise
  */
-int *check_valid_string(const char *b)
+
+int check_valid_string(const char *b)
 {
 if (b == NULL)
 return (0);
@@ -19,6 +21,7 @@ b++;
 }
 return (1);
 }
+
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
  * @b: A pointer to a string of 0 and 1 chars.
@@ -26,6 +29,7 @@ return (1);
  * Return: If b is NULL or contains chars not 0 or 1 - 0.
  *         Otherwise - the converted number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int num = 0;
