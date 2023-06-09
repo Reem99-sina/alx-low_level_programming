01f60310b89cad719d1c4803312553fe8ad067ba
Reem99-sina <reemsina2@gmail.com>
2023-06-09 10:56:51 -0700

    print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ *
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
+ */
+void hash_table_print(hash_table_t *ht)
+{
+ht_item *node;
+unsigned long int i = 0;
+unsigned char comma_flag = 0;
+if (ht == NULL)
+{
+return;
+}
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+if (ht->array[i] != NULL)
+{
+if (comma_flag == 1)
+{
+printf(", ");
+}
+node = ht->array[i];
+while (node != NULL)
+{
+printf("'%s': '%s'", node->key, node->value);
+node = node->next;
+}
+comma_flag = 1;
+}
+}
+printf("}\n");
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,5 +35,5 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, char *key, char *value);
 char *hash_table_get(hash_table_t *ht, char *key);
-
+void hash_table_print(hash_table_t *ht);
 #endif
