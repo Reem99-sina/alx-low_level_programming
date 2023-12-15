244de25a25e2b2a326e6090890890de73c9b73b2
Reem99-sina <reemsina2@gmail.com>
2023-12-15 09:59:12 -0800

    search advance

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,49 +1,54 @@
-#include <stdio.h>
 #include "search_algos.h"
-int recursive_binary_search(int *array, int low, int high, int value)
-{
-if (low <= high)
-{
-int mid = low + (high - low) / 2;
-int i;
-printf("Searching in array:");
-for (i = low; i <= high; ++i)
-{
-printf(" %d", array[i]);
-if (i < high)
-{
-printf(",");
-}
-}
-printf("\n");
 
-if (array[mid] == value)
+/**
+  * advanced_binary_recursive - Searches recursively for a value in a sorted
+  *                             array of integers using binary search.
+  * @array: A pointer to the first element of the [sub]array to search.
+  * @left: The starting index of the [sub]array to search.
+  * @right: The ending index of the [sub]array to search.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
 {
-while (mid > low && array[mid - 1] == value)
-{
-mid--;
-}
-return mid;
-}
-else if (array[mid] < value)
-{
-return recursive_binary_search(array, mid + 1, high, value);
-}
-else
-{
-return recursive_binary_search(array, low, mid - 1, value);
-}
-}
+	size_t i;
 
-return -1;
+	if (right < left)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
 }
 
+/**
+  * advanced_binary - Searches for a value in a sorted array
+  *                   of integers using advanced binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the first index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int advanced_binary(int *array, size_t size, int value)
 {
-if (array == NULL)
-{
-return -1;
-}
+	if (array == NULL || size == 0)
+		return (-1);
 
-return recursive_binary_search(array, 0, size - 1, value);
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,41 +1,46 @@
-#include <stdio.h>
-#include <math.h>
 #include "search_algos.h"
 
 /**
- * linear_skip - Searches for a value in a sorted skip list of integers.
- * @list: Pointer to the head of the skip list to search in.
- * @value: Value to search for.
+ * linear_skip - Searches for an algorithm in a sorted singly
+ *               linked list of integers using linear skip.
+ * @list: A pointer to the  head of the linked list to search.
+ * @value: The value to search for.
  *
- * Return: A pointer on the first node where value is located.
- * If value is not present in list or if list is NULL, return NULL.
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-skiplist_t *current, *express;
+	skiplist_t *node, *jump;
 
-if (list == NULL)
-return NULL;
+	if (list == NULL)
+		return (NULL);
 
-express = list;
-while (express->express != NULL && express->express->n < value)
-{
-printf("Value checked at index [%lu] = [%d]\n",express->express->index, express->express->n);
-express = express->express;
-}
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
 
-printf("Value found between indexes [%lu] and [%lu]\n",express->index, express->express->index);
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-current = express;
-while (current != NULL && current->n <= value)
-{
-printf("Value checked at index [%lu] = [%d]\n",current->index, current->n);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
 
-if (current->n == value)
-return current;
-
-current = current->next;
-}
-
-return NULL;
+	return (node->n == value ? node : NULL);
 }
